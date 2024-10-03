1eae445e8d4600292045211a16ed2961ce3e3c5a
zatzoid <zatzoid@gmail.com>
2024-10-03 16:05:48 +0300

    main

--- src/js/dev/zatz.js
+++ src/js/dev/zatz.js
@@ -1,20 +1,62 @@
 import $ from 'jquery'
-import Inputmask from 'inputmask'
+import Swiper from 'swiper'
+import { Navigation, Pagination } from 'swiper/modules'
+import { rem } from '../utils/constants'
+import initForms from '../utils/forms'
 
 
+$(function () {
+    initSwipers()
+    initForms()
+})
 
-const inputElement = document.getElementById('myInput');
+function initSwipers() {
+    const ourProjects = document.querySelector('.our-projects')
+    if (ourProjects) {
+        new Swiper(ourProjects.querySelector('.swiper'), {
+            loop: true,
+            modules: [Navigation],
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            centeredSlides: true,
+            initialSlide: 3,
+            spaceBetween: rem(3),
+            slideActiveClass: 'our-projects-item_active',
+            slideNextClass: 'our-projects-item_prev',
+            slidePrevClass: 'our-projects-item_next',
+            breakpoints: {
+                768: {
+                    slidesPerView: 4,
+                    spaceBetween: 0,
 
-const phoneMask = new Inputmask("(999) 999-9999");
-const dateMask = new Inputmask("99/99/9999");
+                }
+            },
+            navigation: {
+                prevEl: ourProjects.querySelector('.swiper-btn-prev'),
+                nextEl: ourProjects.querySelector('.swiper-btn-next'),
+            }
 
-document.getElementById('setPhoneMask').addEventListener('click', () => {
-    console.log('q');
-    phoneMask.mask(inputElement); // Применить маску телефона
-});
 
-document.getElementById('setDateMask').addEventListener('click', () => {
-    console.log('2');
+        })
 
-    dateMask.mask(inputElement); // Применить маску даты
-});
+    }
+    const lastNews = document.querySelector('.news');
+    if (lastNews) {
+        new Swiper(lastNews.querySelector('.swiper'), {
+            modules: [Navigation],
+            loop: false,
+            slidesPerView: 1,
+            spaceBetween: rem(3),
+            breakpoints: {
+                768: {
+                    slidesPerView: 3
+                }
+            },
+            navigation: {
+                prevEl: lastNews.querySelector('.swiper-btn-prev'),
+                nextEl: lastNews.querySelector('.swiper-btn-next'),
+            }
+        })
+    }
+}
+
